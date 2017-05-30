26bed078bf4223bdf40639e8928ebe15b8469191
Dan Manastireanu <danmana11@yahoo.com>
2017-05-30 23:40:32 +0300

    added row clearing and score

--- tetris.js
+++ tetris.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     //Block shapes
     var SHAPES = {
         I: [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
@@ -13,6 +13,7 @@
     //Define 10x20 grid as the board
     var GRID_W = 10;
     var GRID_H = 20;
+    var EMPTY_ROW = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     var EMPTY_GRID = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -44,10 +45,11 @@
 
         this.score = 0;
         this.lost = false;
+        this.won = false;
     }
 
 
-    Tetris.prototype.clone = function() {
+    Tetris.prototype.clone = function () {
         var clone = new Tetris();
 
         clone.grid = clone(this.grid);
@@ -56,10 +58,19 @@
 
         clone.score = this.score;
         clone.lost = this.lost;
+        clone.won = this.won;
     };
 
-    Tetris.prototype.toHtml = function() {
+    Tetris.prototype.toHtml = function () {
         var html = '<div class="tetris">', i, j;
+
+        html += '<h1>Score: ' + this.score + '</h1>';
+
+        if (this.lost) {
+            html += '<h2>LOST!</h2>';
+        } else if (this.won) {
+            html += '<h2>WON!</h2>';
+        }
 
         for (i = 0; i < GRID_H; i++) {
             html += '<div class="tetris-row">';
@@ -75,44 +86,107 @@
     };
 
 
-    Tetris.prototype.makeMove = function(x, rot) {
+    Tetris.prototype.makeMove = function (x, rot) {
         var shape = clone(SHAPES[this.nextShapes[this.shapeIndex]]), i;
 
         shape = rotate(shape, rot);
 
-        var i, dropPosition = this.grid.length - 1;
-        for (i = -shape.length; i < this.grid.length; i++) {
-            if (collides(this.grid, shape, x, i)) {
-                dropPosition = i - 1;
-                break;
+        var y = getDropLocation(this.grid, shape, x);
+
+        if (isLoss(shape, y)) {
+            this.lost = true;
+        } else {
+            this.score++;
+            applyShape(this.grid, shape, x, y);
+            this.score += clearRows(this.grid);
+
+            this.shapeIndex++;
+            if (this.shapeIndex >= this.nextShapes.length) {
+                this.won = true;
+            }
+        }
+    };
+
+    function clearRows(grid) {
+        var i, j, cleared = 0, is_full, score = 0;
+        for (i = 0; i < GRID_H; i++) {
+            is_full = true;
+            for (j = 0; j < GRID_W; j++) {
+                if (grid[i][j] === 0) {
+                    is_full = false;
+                    break;
+                }
+            }
+            if (is_full) {
+                cleared++;
+                grid.splice(i, 1);
+                grid.unshift(clone(EMPTY_ROW));
             }
         }
 
-        // apply the shape
+        if (cleared === 1) {
+            score = 100;
+        } else if (cleared === 2) {
+            score = 200;
+        } else if (cleared === 3) {
+            score = 400;
+        } else if (cleared >= 4) {
+            score = 800;
+        }
+
+        return score;
+
+
+    }
+
+    function getDropLocation(grid, shape, x) {
+        var i;
+        for (i = -shape.length; i < GRID_H; i++) {
+            if (collides(grid, shape, x, i)) {
+                return i - 1;
+            }
+        }
+
+        return GRID_H - 1;
+    }
+
+    function isLoss(shape, y) {
+        var i, j;
         for (i = 0; i < shape.length; i++) {
-            for (j = 0; j < shape[i].length; j++) {
-                if (shape[i][j] && x + j < GRID_W && dropPosition + i >= 0 && dropPosition + i < GRID_H) {
-                    this.grid[dropPosition + i][x + j] = shape[i][j];
+            for (j = 0; j < shape.length; j++) {
+                if (shape[i][j] && y + i < 0) {
+                    return true;
                 }
             }
         }
+        return false;
+    }
 
-        this.shapeIndex++;
-    };
+    function applyShape(grid, shape, x, y) {
+        for (i = 0; i < shape.length; i++) {
+            for (j = 0; j < shape[i].length; j++) {
+                if (shape[i][j]) {
+                    if (x + j < GRID_W && y + i >= 0 && y + i < GRID_H) {
+                        grid[y + i][x + j] = shape[i][j];
+                    }
+                }
+            }
+        }
+    }
 
     function collides(grid, shape, x, y) {
         var i, j;
         for (i = 0; i < shape.length; i++) {
             for (j = 0; j < shape[i].length; j++) {
                 if (shape[i][j]) {
-                    if (x +j < 0 || x + j >= GRID_W) {
+                    if (x + j < 0 || x + j >= GRID_W) {
                         return true;
                     }
                     if (y + i >= GRID_H) {
                         return true;
                     }
-                    if (y + i >=0) {
-                        if (grid[y+i][x+j] !== 0) {
+                    if (y + i >= 0) {
+                        if (grid[y + i][x + j] !== 0) {
                             return true;
                         }
                     }
@@ -146,8 +220,8 @@
 
 //flip row x column to column x row
     function transpose(array) {
-        return array[0].map(function(col, i) {
-            return array.map(function(row) {
+        return array[0].map(function (col, i) {
+            return array.map(function (row) {
                 return row[i];
             });
         });
