d0b85f63e906e16e31bdcef8d7290e322d3be354
Dan.Manastireanu <Dan.Manastireanu@isdc.eu>
2017-06-08 18:38:23 +0300

    Add shape visualizations with all possible rotations

--- app/intro/intro.js
+++ app/intro/intro.js
@@ -11,6 +11,23 @@ angular.module('tetrisbot.intro', ['ngRoute'])
 
 .controller('IntroCtrl', ['$scope', function($scope) {
 
-	
+  $scope.SHAPE_NAMES = Tetris.SHAPE_NAMES;
+
+  function showShape(name) {
+    var shape = Tetris.clone(Tetris.SHAPES[name]), i, html = '';
+
+    for (i = 0; i < 4; i++) {
+      html += '<div class="shape-demo">' + Tetris.gridToHTML(Tetris.rotate(shape, i)) + '</div>';
+    }
+    console.log(html);
+
+    document.getElementById('shape-demo-' + name).innerHTML = html;
+  }
+
+  var i;
+  for (i = 0; i < Tetris.SHAPE_NAMES.length; i++) {
+    showShape(Tetris.SHAPE_NAMES[i]);
+  }
+
 
 }]);
--- tetris.js
+++ tetris.js
@@ -1,239 +1,251 @@
 (function() {
-    //Block shapes
-    var SHAPES = {
-        I: [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
-        J: [[2, 0, 0], [2, 2, 2], [0, 0, 0]],
-        L: [[0, 0, 3], [3, 3, 3], [0, 0, 0]],
-        O: [[4, 4], [4, 4]],
-        S: [[0, 5, 5], [5, 5, 0], [0, 0, 0]],
-        T: [[0, 6, 0], [6, 6, 6], [0, 0, 0]],
-        Z: [[7, 7, 0], [0, 7, 7], [0, 0, 0]]
-    };
+  //Block shapes
+  var SHAPES = {
+    I: [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+    J: [[2, 0, 0], [2, 2, 2], [0, 0, 0]],
+    L: [[0, 0, 3], [3, 3, 3], [0, 0, 0]],
+    O: [[4, 4], [4, 4]],
+    S: [[0, 5, 5], [5, 5, 0], [0, 0, 0]],
+    T: [[0, 6, 0], [6, 6, 6], [0, 0, 0]],
+    Z: [[7, 7, 0], [0, 7, 7], [0, 0, 0]]
+  };
 
-    //Define 10x20 grid as the board
-    var GRID_W = 10;
-    var GRID_H = 20;
-    var EMPTY_ROW = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    var EMPTY_GRID = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
+  //Define 10x20 grid as the board
+  var GRID_W = 10;
+  var GRID_H = 20;
+  var EMPTY_ROW = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var EMPTY_GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
 
-    function Tetris(nextShapes) {
-        this.grid = clone(EMPTY_GRID);
+  function Tetris(nextShapes) {
+    this.grid = clone(EMPTY_GRID);
 
-        this.nextShapes = nextShapes || '';
-        this.shapeIndex = 0;
+    this.nextShapes = nextShapes || '';
+    this.shapeIndex = 0;
 
-        this.score = 0;
-        this.lost = false;
-        this.won = false;
+    this.score = 0;
+    this.lost = false;
+    this.won = false;
+  }
+
+  Tetris.prototype.clone = function() {
+    var clone = new Tetris();
+
+    clone.grid = clone(this.grid);
+    clone.nextShapes = this.nextShapes;
+    clone.shapeIndex = this.shapeIndex;
+
+    clone.score = this.score;
+    clone.lost = this.lost;
+    clone.won = this.won;
+  };
+
+  Tetris.prototype.toHtml = function() {
+    var html = '<div class="tetris">';
+
+    html += '<h1>Score: ' + this.score;
+
+    if (this.lost) {
+      html += '<span class="lost">LOST!</span>';
+    } else if (this.won) {
+      html += '<span class="won">WON!</span>';
     }
 
-    Tetris.SHAPE_NAMES = 'IJLOSTZ';
-    Tetris.SHAPES = SHAPES;
+    html += '</h1>';
+
+    html += gridToHtml(this.grid);
+
+    html += '</div>';
+    return html;
+  };
+
+  function gridToHtml(grid) {
+    var i, j, row, html = '';
+    for (i = 0; i < grid.length; i++) {
+      row = grid[i];
+      html += '<div class="tetris-row">';
+      for (j = 0; j < row.length; j++) {
+        html += '<div class="tetris-cell tetris-cell-' + row[j] + '"></div>';
+      }
+      html += '</div>';
+    }
+
+    return html;
+  }
 
 
-    Tetris.prototype.clone = function() {
-        var clone = new Tetris();
+  Tetris.prototype.makeMove = function(x, rot) {
+    var shape = clone(SHAPES[this.nextShapes[this.shapeIndex]]), i;
 
-        clone.grid = clone(this.grid);
-        clone.nextShapes = this.nextShapes;
-        clone.shapeIndex = this.shapeIndex;
+    shape = rotate(shape, rot);
 
-        clone.score = this.score;
-        clone.lost = this.lost;
-        clone.won = this.won;
-    };
+    var y = getDropLocation(this.grid, shape, x);
 
-    Tetris.prototype.toHtml = function() {
-        var html = '<div class="tetris">', i, j;
+    if (isLoss(shape, y)) {
+      this.lost = true;
+    } else {
+      this.score++;
+      applyShape(this.grid, shape, x, y);
+      this.score += clearRows(this.grid);
 
-        html += '<h1>Score: ' + this.score;
+      this.shapeIndex++;
+      if (this.shapeIndex >= this.nextShapes.length) {
+        this.won = true;
+      }
+    }
+  };
 
-        if (this.lost) {
-            html += '<span class="lost">LOST!</span>';
-        } else if (this.won) {
-            html += '<span class="won">WON!</span>';
+  function clearRows(grid) {
+    var i, j, cleared = 0, is_full, score = 0;
+    for (i = 0; i < GRID_H; i++) {
+      is_full = true;
+      for (j = 0; j < GRID_W; j++) {
+        if (grid[i][j] === 0) {
+          is_full = false;
+          break;
         }
+      }
+      if (is_full) {
+        cleared++;
+        grid.splice(i, 1);
+        grid.unshift(clone(EMPTY_ROW));
+      }
+    }
 
-        html += '</h1>';
+    if (cleared === 1) {
+      score = 100;
+    } else if (cleared === 2) {
+      score = 200;
+    } else if (cleared === 3) {
+      score = 400;
+    } else if (cleared >= 4) {
+      score = 800;
+    }
 
-        for (i = 0; i < GRID_H; i++) {
-            html += '<div class="tetris-row">';
-            for (j = 0; j < GRID_W; j++) {
-                html += '<div class="tetris-cell tetris-cell-' + this.grid[i][j] + '"></div>';
+    return score;
+
+
+  }
+
+  function getDropLocation(grid, shape, x) {
+    var i;
+    for (i = -shape.length; i < GRID_H; i++) {
+      if (collides(grid, shape, x, i)) {
+        return i - 1;
+      }
+    }
+
+    return GRID_H - 1;
+  }
+
+  function isLoss(shape, y) {
+    var i, j;
+    for (i = 0; i < shape.length; i++) {
+      for (j = 0; j < shape.length; j++) {
+        if (shape[i][j] && y + i < 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  function applyShape(grid, shape, x, y) {
+    for (i = 0; i < shape.length; i++) {
+      for (j = 0; j < shape[i].length; j++) {
+        if (shape[i][j]) {
+          if (x + j < GRID_W && y + i >= 0 && y + i < GRID_H) {
+            grid[y + i][x + j] = shape[i][j];
+          }
+        }
+      }
+    }
+  }
+
+  function collides(grid, shape, x, y) {
+    var i, j;
+    for (i = 0; i < shape.length; i++) {
+      for (j = 0; j < shape[i].length; j++) {
+        if (shape[i][j]) {
+          if (x + j < 0 || x + j >= GRID_W) {
+            return true;
+          }
+          if (y + i >= GRID_H) {
+            return true;
+          }
+          if (y + i >= 0) {
+            if (grid[y + i][x + j] !== 0) {
+              return true;
             }
-            html += '</div>';
+          }
         }
-
-
-        html += '</div>';
-        return html;
-    };
-
-
-    Tetris.prototype.makeMove = function(x, rot) {
-        var shape = clone(SHAPES[this.nextShapes[this.shapeIndex]]), i;
-
-        shape = rotate(shape, rot);
-
-        var y = getDropLocation(this.grid, shape, x);
-
-        if (isLoss(shape, y)) {
-            this.lost = true;
-        } else {
-            this.score++;
-            applyShape(this.grid, shape, x, y);
-            this.score += clearRows(this.grid);
-
-            this.shapeIndex++;
-            if (this.shapeIndex >= this.nextShapes.length) {
-                this.won = true;
-            }
-        }
-    };
-
-    function clearRows(grid) {
-        var i, j, cleared = 0, is_full, score = 0;
-        for (i = 0; i < GRID_H; i++) {
-            is_full = true;
-            for (j = 0; j < GRID_W; j++) {
-                if (grid[i][j] === 0) {
-                    is_full = false;
-                    break;
-                }
-            }
-            if (is_full) {
-                cleared++;
-                grid.splice(i, 1);
-                grid.unshift(clone(EMPTY_ROW));
-            }
-        }
-
-        if (cleared === 1) {
-            score = 100;
-        } else if (cleared === 2) {
-            score = 200;
-        } else if (cleared === 3) {
-            score = 400;
-        } else if (cleared >= 4) {
-            score = 800;
-        }
-
-        return score;
-
+      }
 
     }
 
-    function getDropLocation(grid, shape, x) {
-        var i;
-        for (i = -shape.length; i < GRID_H; i++) {
-            if (collides(grid, shape, x, i)) {
-                return i - 1;
-            }
-        }
+    return false;
+  }
 
-        return GRID_H - 1;
+  // clones an object
+  function clone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+
+
+  //for rotating a shape, how many times should we rotate
+  function rotate(matrix, times) {
+    //for each time
+    for (var t = 0; t < times; t++) {
+      //flip the shape matrix
+      matrix = transpose(matrix);
+      //and for the length of the matrix, reverse each column
+      for (var i = 0; i < matrix.length; i++) {
+        matrix[i].reverse();
+      }
     }
-
-    function isLoss(shape, y) {
-        var i, j;
-        for (i = 0; i < shape.length; i++) {
-            for (j = 0; j < shape.length; j++) {
-                if (shape[i][j] && y + i < 0) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    function applyShape(grid, shape, x, y) {
-        for (i = 0; i < shape.length; i++) {
-            for (j = 0; j < shape[i].length; j++) {
-                if (shape[i][j]) {
-                    if (x + j < GRID_W && y + i >= 0 && y + i < GRID_H) {
-                        grid[y + i][x + j] = shape[i][j];
-                    }
-                }
-            }
-        }
-    }
-
-    function collides(grid, shape, x, y) {
-        var i, j;
-        for (i = 0; i < shape.length; i++) {
-            for (j = 0; j < shape[i].length; j++) {
-                if (shape[i][j]) {
-                    if (x + j < 0 || x + j >= GRID_W) {
-                        return true;
-                    }
-                    if (y + i >= GRID_H) {
-                        return true;
-                    }
-                    if (y + i >= 0) {
-                        if (grid[y + i][x + j] !== 0) {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-        }
-
-        return false;
-    }
-
-    // clones an object
-    function clone(obj) {
-        return JSON.parse(JSON.stringify(obj));
-    }
-
-
-    //for rotating a shape, how many times should we rotate
-    function rotate(matrix, times) {
-        //for each time
-        for (var t = 0; t < times; t++) {
-            //flip the shape matrix
-            matrix = transpose(matrix);
-            //and for the length of the matrix, reverse each column
-            for (var i = 0; i < matrix.length; i++) {
-                matrix[i].reverse();
-            }
-        }
-        return matrix;
-    }
+    return matrix;
+  }
 
 //flip row x column to column x row
-    function transpose(array) {
-        return array[0].map(function(col, i) {
-            return array.map(function(row) {
-                return row[i];
-            });
-        });
-    }
+  function transpose(array) {
+    return array[0].map(function(col, i) {
+      return array.map(function(row) {
+        return row[i];
+      });
+    });
+  }
 
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-        module.exports = Tetris;
-    else 
-        window.Tetris = Tetris;
+  // Expose some of the static utility functions
+
+  Tetris.SHAPE_NAMES = 'IJLOSTZ';
+  Tetris.SHAPES = SHAPES;
+  Tetris.rotate = rotate;
+  Tetris.transpose = transpose;
+  Tetris.gridToHTML = gridToHtml;
+  Tetris.clone = clone;
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = Tetris;
+  else
+    window.Tetris = Tetris;
 }());
