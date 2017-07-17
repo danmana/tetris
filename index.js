70337174704c6f32214284707ec111b12ee9f61d
Dan.Manastireanu <Dan.Manastireanu@isdc.eu>
2017-07-17 17:13:56 +0300

    Added parameters for port and base dir

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 /* App necesities */
-var express  = require('express');
-var app      = express();                   
-var morgan = require('morgan');            
-var bodyParser = require('body-parser');    
-var methodOverride = require('method-override'); 
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+
+var args = require('minimist')(process.argv.slice(2), {
+  alias: {
+    'p': 'port',
+    'd': 'dir',
+    'h': 'help'
+  }
+});
+
+var port = args.port || 8099;
+var baseDir = args.dir || "../tmp";
+
+if (args.h || args.help) {
+  console.log('usage: node index.js [-p 80] [-d ../tmp] [-h] \n\n' +
+  'Arguments:\n' +
+  '  -p, --port 8099         sets the server port\n' +
+  '  -d, --dir ../tmp        sets the directory where to store submissions\n' +
+  '  -h, --help              prints this help message');
+  return;
+}
+
+console.log('Using port', port);
+console.log('Using dir', baseDir);
+
 
 /* Disk storage necesities */
-var baseDir = "/home/osboxes/tmp";
+
 var scoreFile = baseDir + "/score.json";
 var mkdirp = require('mkdirp');
-var multer  = require('multer');
+var multer = require('multer');
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-  	var directory = baseDir + "/" + req.body.username;
-  	mkdirp.sync(directory);
+  destination: function(req, file, cb) {
+    var directory = baseDir + "/" + req.body.username;
+    mkdirp.sync(directory);
     cb(null, directory);
   },
-  filename: function (req, file, cb) {
-  	var filename = req.body.type + '-' + Date.now() + '-' + file.originalname;
+  filename: function(req, file, cb) {
+    var filename = req.body.type + '-' + Date.now() + '-' + file.originalname;
     cb(null, filename);
   }
 });
-var upload = multer({ storage: storage })
+var upload = multer({storage: storage})
 
 /* CSV parsing necesities */
 var csv = require('csv-parser')
@@ -32,36 +57,36 @@ var tetris = require('./tetris-helper.js');
 
 /* App config */
 app.use(express.static(__dirname + '/app'));
-app.use(express.static(__dirname));                  
-app.use(morgan('dev'));                                        
-app.use(bodyParser.urlencoded({'extended':'true'}));           
-app.use(bodyParser.json());                                     
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(express.static(__dirname));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
 
 app.get('/', function(req, res) {
-	res.sendfile('./app/index.html'); 
+  res.sendfile('./app/index.html');
 });
 
-app.post('/top-challengers',  function(req, res) {
-	var scores = JSON.parse(fs.readFileSync(scoreFile).toString());
-	var sortable = [];
-	
-	for (var username in scores) {
-    	sortable.push([username, Math.max.apply(null, scores[username]), scores[username].length]);
-	}
+app.post('/top-challengers', function(req, res) {
+  var scores = JSON.parse(fs.readFileSync(scoreFile).toString());
+  var sortable = [];
 
-	sortable.sort(function(a, b) {
-    	return b[1] - a[1];
-	});
+  for (var username in scores) {
+    sortable.push([username, Math.max.apply(null, scores[username]), scores[username].length]);
+  }
 
-	var response = [];
-	for (var i=0; i<sortable.length; i++){
-		var entry = sortable[i];
-		response.push({email: entry[0], score: entry[1], attempts: entry[2]});
-	}
-	res.json(response);
+  sortable.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+
+  var response = [];
+  for (var i = 0; i < sortable.length; i++) {
+    var entry = sortable[i];
+    response.push({email: entry[0], score: entry[1], attempts: entry[2]});
+  }
+  res.json(response);
 });
 
 /* Basic necesities for storing the data */
@@ -69,32 +94,31 @@ mkdirp.sync(baseDir);
 if (!fs.existsSync(scoreFile)) fs.writeFileSync(scoreFile, "{}");
 
 /* Results storage & evaluator */
-app.post('/upload-results', upload.single('results'), function (req, res, next) {
+app.post('/upload-results', upload.single('results'), function(req, res, next) {
 
-	var username = req.body.username;
-	var scores = JSON.parse(fs.readFileSync(scoreFile).toString());
-	var existings = scores[username] || [];
+  var username = req.body.username;
+  var scores = JSON.parse(fs.readFileSync(scoreFile).toString());
+  var existings = scores[username] || [];
 
-	var score = 0;
-	fs.createReadStream(req.file.destination + "/" + req.file.filename)
-  	  .pipe(csv())
-      .on('data', function (data) {
-    	score = score + tetris.getScore(data.game, tetris.moveParser(data.moves));
-  	  })
-  	  .on('end', function () {
-  	  	existings.push(score);
-  	  	scores[username] = existings;
-  	  	fs.writeFileSync(scoreFile, JSON.stringify(scores));
-  	  	res.send({ score: score });
-  	  });
-    
+  var score = 0;
+  fs.createReadStream(req.file.destination + "/" + req.file.filename)
+  .pipe(csv())
+  .on('data', function(data) {
+    score = score + tetris.getScore(data.game, tetris.moveParser(data.moves));
+  })
+  .on('end', function() {
+    existings.push(score);
+    scores[username] = existings;
+    fs.writeFileSync(scoreFile, JSON.stringify(scores));
+    res.send({score: score});
+  });
+
 });
 
 
-app.post('/upload-solution', upload.single('solution'), function (req, res, next) {
-	res.sendStatus(201);
+app.post('/upload-solution', upload.single('solution'), function(req, res, next) {
+  res.sendStatus(201);
 });
 
-
-app.listen(8099);
-console.log("App listening on port 8099");
+app.listen(port);
+console.log("App listening on port " + port);
