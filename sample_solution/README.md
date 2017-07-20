88ce833d3a13788264624497ce967dc7dd199631
Dan.Manastireanu <Dan.Manastireanu@isdc.eu>
2017-07-20 13:25:47 +0300

    Add tetris.js description

--- sample_solution/README.md
+++ sample_solution/README.md
@@ -2,7 +2,6 @@
 
 This folder contains a solution skeleton that you can extend with your own algorithm.
 
-
 To run the solution first install all dependencies with:
 ```
 npm install
@@ -14,3 +13,89 @@ node solution.js
 ```
 
 This will output your solution in the `solutions` folder.
+
+## Tetris.js
+
+The Tetris engine used for scoring can also be used for developing your solution.
+
+You can find the js implementation [here](https://github.com/danmana/tetris/blob/master/tetris.js).
+
+### Tetris.js API
+
+To create a new game all you have to do is pass in the shapes.
+The game will keep track of the current position, the grid cells and your score as you make moves.
+
+The Tetris engine maintains the following internal state:
+ * `shapes` (string) - the list of input shapes
+ * `shapeIndex` (number) - the current shape index
+ * `grid` (number[][])- the game grid, a 20 by 10 array of cells (0 = empty, 1-7 part of a shape)
+ * `score` (number)- current score
+ * `lost` (boolean) - if the game is lost (the last shape you placed could not fit in the current grid)
+ * `won` (boolean) - if the game is won (you finished all shapes and didn't go outside of the grid)
+
+
+
+```js
+// creating a new game
+var t = new Tetris('LSTSITS');
+
+// the current shape index (starts at 0 and increments automatically for each move)
+console.log(t.shapeIndex); // 0
+
+// making a move (place the current shape at column 5, rotated 0 times)
+t.makeMove(5,0);
+console.log(t.shapeIndex); // 1
+
+// check the current game grid to see what cells are filled
+console.log(t.grid); // 20 x 10 array - 0=empty cell, 1-7=belongs to one of the shapes
+
+// check if you won or lost
+if (t.won) {
+  console.log('You won!, score = ' + t.score);
+} else if (t.lost) {
+  console.log('You lost :( score = ' + t.score);
+} else {
+  console.log('Game in progress ... score = ' + t.score);
+}
+
+// clone the game if you want to try out a move without advancing the game
+var t2 = t.clone();
+t2.makeMove(5,0);
+console.log(t2.score);
+
+```
+
+#### Constants
+```js
+// tetris shape names: 'IJLOSTZ'
+Tetris.SHAPE_NAMES;
+
+// a map of shapes <shape_name,shape_grid>
+Tetris.SHAPES;
+
+// grid width (10)
+Tetris.GRID_W;
+
+// grid height (20)
+Tetris.GRID_H;
+```
+
+#### Utility functions
+```js
+// util for rotating shapes
+Tetris.rotate(shape, times);
+
+// util for transposing an array
+Tetris.transpose(array);
+
+// transform any grid into html (can be used on shapes or the entire grid)
+// wrap this with a <div class="tetris"></div> to use the css styles from tetris.css
+Tetris.gridToHTML(grid);
+
+// clone any plain object or array
+Tetris.clone(obj);
+
+// parse moves string into an array of internal moves: {x: number; rot: number;}
+Tetris.parseMoves(moves);
+```
+
--- tetris.js
+++ tetris.js
@@ -37,6 +37,11 @@
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
 
+  /**
+   * Create a new Tetris instance.
+   * @param nextShapes the game shapes
+   * @constructor
+   */
   function Tetris(nextShapes) {
     this.grid = clone(EMPTY_GRID);
 
@@ -48,6 +53,9 @@
     this.won = false;
   }
 
+  /**
+   * Clone this instance of Tetris.
+   */
   Tetris.prototype.clone = function() {
     var clone = new Tetris();
 
@@ -60,6 +68,10 @@
     clone.won = this.won;
   };
 
+  /**
+   * Output a html string representing the current state of this Tetris game.
+   * @returns {string}
+   */
   Tetris.prototype.toHtml = function() {
     var html = '<div class="tetris">';
 
@@ -79,6 +91,12 @@
     return html;
   };
 
+  /**
+   * Convert a grid (2 dimensional array) to html.
+   * Wrap this in a <div class="tetris"></div> to use the tetris.css styles
+   * @param grid
+   * @returns {string}
+   */
   function gridToHtml(grid) {
     var i, j, row, html = '';
     for (i = 0; i < grid.length; i++) {
@@ -94,6 +112,11 @@
   }
 
 
+  /**
+   * Make a move. This will alter the current state of the game.
+   * @param x the column where to place the next shape
+   * @param rot the numebr of rotations to apply to the next shape
+   */
   Tetris.prototype.makeMove = function(x, rot) {
     var shape = clone(SHAPES[this.nextShapes[this.shapeIndex]]), i;
 
@@ -119,6 +142,11 @@
     }
   };
 
+  /**
+   * Clear all filled rows
+   * @param grid
+   * @returns {number} the number of rows cleared
+   */
   function clearRows(grid) {
     var i, j, cleared = 0, is_full, score = 0;
     for (i = 0; i < GRID_H; i++) {
@@ -151,6 +179,13 @@
 
   }
 
+  /**
+   * Get the row where this shape will drop
+   * @param grid
+   * @param shape
+   * @param x
+   * @returns {number}
+   */
   function getDropLocation(grid, shape, x) {
     var i;
     for (i = -shape.length; i < GRID_H; i++) {
@@ -162,6 +197,12 @@
     return GRID_H - 1;
   }
 
+  /**
+   * Check if placing the shape at this row causes a loss (the shape is outside the grid)
+   * @param shape
+   * @param y
+   * @returns {boolean}
+   */
   function isLoss(shape, y) {
     var i, j;
     for (i = 0; i < shape.length; i++) {
@@ -174,6 +215,13 @@
     return false;
   }
 
+  /**
+   * Paste the given shape in the grid at x,y starting with the top,left corner of the shape.
+   * @param grid
+   * @param shape
+   * @param x
+   * @param y
+   */
   function applyShape(grid, shape, x, y) {
     for (i = 0; i < shape.length; i++) {
       for (j = 0; j < shape[i].length; j++) {
@@ -186,6 +234,11 @@
     }
   }
 
+  /**
+   * Get the min/max positions where this shape could be placed such that it doesn't go outside the gird.
+   * @param shape
+   * @returns {{min: number, max: number}}
+   */
   function getShapePositionBounds(shape) {
     var i, j;
     var minX = GRID_W;
@@ -199,9 +252,15 @@
       }
     }
 
-    return {min: -minX, max: GRID_W - 1 - maxX };
+    return {min: -minX, max: GRID_W - 1 - maxX};
   }
 
+  /**
+   * Bound the x position based on the min/max posiion where the shape can be placed (such that it doesn't go outside the grid)
+   * @param shape
+   * @param x
+   * @returns {x: number} bounded x position
+   */
   function getBoundedX(shape, x) {
     var bounds = getShapePositionBounds(shape);
     if (x < bounds.min) {
@@ -213,6 +272,16 @@
     return x;
   }
 
+  /**
+   * Check if placing the given shape at position x,y will collide with the grid or has filled cells outside the grid.
+   * x,y represent where to place the top, left cell of the shape
+   *
+   * @param grid the grid to test on
+   * @param shape the shape to try and place
+   * @param x the column position where to place the shape
+   * @param y the row where to place it (0 = on top, can be negative, GRID_H-1 on bottom)
+   * @returns {boolean} if the shape collides with anything on the grid
+   */
   function collides(grid, shape, x, y) {
     var i, j;
     for (i = 0; i < shape.length; i++) {
@@ -237,13 +306,20 @@
     return false;
   }
 
-  // clones an object
+  /**
+   * Clone any plain object or array.
+   */
   function clone(obj) {
     return JSON.parse(JSON.stringify(obj));
   }
 
 
-  //for rotating a shape, how many times should we rotate
+  /**
+   * Rotate a shape or grid.
+   * @param matrix the 2 dimensional array to rotate
+   * @param times the number of rotations
+   * @returns {*}
+   */
   function rotate(matrix, times) {
     //for each time
     for (var t = 0; t < times; t++) {
@@ -257,7 +333,11 @@
     return matrix;
   }
 
-//flip row x column to column x row
+  /**
+   * Transpose a matrix (2 dimensional array).
+   * flip row x column to column x row
+   * @param array
+   */
   function transpose(array) {
     return array[0].map(function(col, i) {
       return array.map(function(row) {
